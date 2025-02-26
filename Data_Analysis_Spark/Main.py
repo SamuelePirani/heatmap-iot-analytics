9862fd77aca980b58115f4a4679022bdaf4039b5
Samuele Pirani <samuele.pirani@studenti.unicam.it>
2025-02-27 00:37:16 +0100

    Prototype data collection from spark job

--- Data_Analysis_Spark/Main.py
+++ Data_Analysis_Spark/Main.py
@@ -1,24 +1,35 @@
 from functools import reduce
 import os
+import pandas as pd
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_unixtime, col
 
 spark = SparkSession.builder.appName("HeatMapJob").getOrCreate()
 
 try:
-    os.system("python Python-DataNormalizer\Main.py")
-    csv_file = spark.createDataFrame([])
+    #os.system("python Python-DataNormalizer\Main.py")
+    csv_files = []
+    result = []
     for subdir, dirs, files in os.walk("Data\IotData"):
         for file in files:
             if file.endswith(".csv"):
                 path_name = os.path.join(subdir, file)
-                csv_file = csv_file.append(spark.read.csv(path_name, header=False, inferSchema=True))
-                csv_file.show()
-            
-
+                df = spark.read.csv(path_name, header=False, inferSchema=True)
+                column_names = ["ID_Room", f"TimeStamp_{os.path.splitext(file)[0]}", f"Value_{os.path.splitext(file)[0]}"]
+                df = df.toDF(*column_names)
+                csv_files.append(df)
+        if len(csv_files) != 0:
+            cobiner = csv_files[0]
+            for csv in csv_files[1:]:
+                cobiner = cobiner.join(csv, on="ID_Room", how='inner')
+            csv_files = []
+            result.append(cobiner)
+    result[0].printSchema()
 except Exception as e:
     print(f"Error: {e}")
 finally:
     spark.stop()
+
 
 
 
