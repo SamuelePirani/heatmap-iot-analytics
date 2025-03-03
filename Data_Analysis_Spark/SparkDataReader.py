b4377650bc275e17e0f8a30d08441411ab698386
MacMat01 <matteo.machella01@gmail.com>
2025-03-03 19:21:01 +0100

    replace path finding method

--- Data_Analysis_Spark/SparkDataReader.py
+++ Data_Analysis_Spark/SparkDataReader.py
@@ -1,31 +1,31 @@
 import os
-
+import sys
 import yaml
 
-ROOT = os.getcwd()  
-HOME = os.path.dirname(ROOT)  
+ROOT = sys.path[1]
+print(ROOT)
 
-config_file_path = os.path.join(HOME, "config.yml")
+config_file_path = os.path.join(ROOT, "config.yml")
 
-with open(config_file_path, "r") as file:
+with open(config_file_path) as file:
     config = yaml.safe_load(file)
 
-config["iot_data_path"] = os.path.join(HOME, config["iot_data_path"])
+config["iot_data_path"] = os.path.join(ROOT, config["iot_data_path"])
 
 
 class SparkDataReader(object):
     def __init__(self, spark):
         self.spark_session = spark
 
-    # Read data based on the name provided by the sender class
+
     def read(self, csv_to_read):
         csv_files = []
         for subdir, dirs, files in os.walk(config["iot_data_path"]):
-            for file in files:
-                if file.endswith(csv_to_read):
-                    path_name = os.path.join(subdir, file)
+            for f in files:
+                if f.endswith(csv_to_read):
+                    path_name = os.path.join(subdir, f)
                     df = self.spark_session.read.csv(path_name, header=False, inferSchema=True)
-                    column_names = ["ID_Room", f"Timestamp", f"Value_{os.path.splitext(file)[0]}"]
+                    column_names = ["ID_Room", f"Timestamp", f"Value_{os.path.splitext(f)[0]}"]
                     df = df.toDF(*column_names)
                     csv_files.append(df)
         return csv_files
