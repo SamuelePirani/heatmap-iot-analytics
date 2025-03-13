16783340c3227759e6f4f0880eb10319e18c2b61
MacMat01 <matteo.machella01@gmail.com>
2025-03-13 09:59:08 +0100

    Refactor code for consistent formatting and readability
    Apply consistent code formatting to HTML, SCSS, TypeScript, and JSON files across the project. This improves readability, maintains standardized spacing and conventions, and ensures uniformity in code structure.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,43 +1,42 @@
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { isPlatformBrowser } from '@angular/common';
+import {Component, Inject, PLATFORM_ID} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {isPlatformBrowser} from '@angular/common';
 import GeoJSON from 'ol/format/GeoJSON';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    standalone: false,
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'client';
-  isBrowser: boolean;
+    title = 'client';
+    isBrowser: boolean;
 
-  floors = [
-    { id: 'floor1', name: 'First Floor', geoJsonUrl: './assets/geoJsonFloor/FirstFloor.geojson' },
-    { id: 'floor2', name: 'Second Floor', geoJsonUrl: './assets/geoJsonFloor/SecondFloor.geojson' },
-  ];
+    floors = [
+        {id: 'floor1', name: 'First Floor', geoJsonUrl: './assets/geoJsonFloor/FirstFloor.geojson'},
+        {id: 'floor2', name: 'Second Floor', geoJsonUrl: './assets/geoJsonFloor/SecondFloor.geojson'},
+    ];
+    selectedFloor = this.floors[0].geoJsonUrl;
+    rooms: string[] = [];
+    selectedRoom: string = '';
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object, private http: HttpClient) {
-    this.isBrowser = isPlatformBrowser(this.platformId);
-    this.loadRooms(this.selectedFloor);
-  }
+    constructor(@Inject(PLATFORM_ID) private platformId: Object, private http: HttpClient) {
+        this.isBrowser = isPlatformBrowser(this.platformId);
+        this.loadRooms(this.selectedFloor);
+    }
 
-  selectedFloor = this.floors[0].geoJsonUrl;
-  rooms: string[] = [];
-  selectedRoom: string = '';
+    loadRooms(geoJsonUrl: string) {
+        this.http.get(geoJsonUrl).subscribe((data: any) => {
+            const features = new GeoJSON().readFeatures(data);
+            this.rooms = features
+                .filter((feature: any) => feature.get('type') === 'Room')
+                .map((feature: any) => feature.get('name'));
+        });
+    }
 
-  loadRooms(geoJsonUrl: string) {
-    this.http.get(geoJsonUrl).subscribe((data: any) => {
-      const features = new GeoJSON().readFeatures(data);
-      this.rooms = features
-        .filter((feature: any) => feature.get('type') === 'Room')
-        .map((feature: any) => feature.get('name'));
-    });
-  }
-
-  onFloorChange(event: any) {
-    this.loadRooms(this.selectedFloor);
-    this.selectedRoom = "";
-  }
+    onFloorChange(event: any) {
+        this.loadRooms(this.selectedFloor);
+        this.selectedRoom = "";
+    }
 }
