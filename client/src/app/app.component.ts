aebfdf8509d14ed659a52108e3578ead42588768
MacMat01 <matteo.machella01@gmail.com>
2025-03-13 10:24:28 +0100

    Refactor component structure and enhance maintainability
    Refactored HTML to improve accessibility and readability by replacing repetitive `<div>` tags with `<label>` elements. Introduced SCSS variables and mixins for consistent styling and easier maintenance. Enhanced `HeatmapComponent` with `OnChanges` lifecycle, private methods, and improved type annotations for better code clarity and functionality. Updated `AppComponent` for stricter typing and modular methods like `fetchRooms` to simplify data handling.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,40 +3,50 @@ import {HttpClient} from '@angular/common/http';
 import {isPlatformBrowser} from '@angular/common';
 import GeoJSON from 'ol/format/GeoJSON';
 
+interface Floor {
+    id: string;
+    name: string;
+    geoJsonUrl: string;
+}
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     standalone: false,
-    styleUrl: './app.component.scss'
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-    title = 'client';
     isBrowser: boolean;
 
-    floors = [
+    floors: Floor[] = [
         {id: 'floor1', name: 'First Floor', geoJsonUrl: './assets/geoJsonFloor/FirstFloor.geojson'},
         {id: 'floor2', name: 'Second Floor', geoJsonUrl: './assets/geoJsonFloor/SecondFloor.geojson'},
     ];
-    selectedFloor = this.floors[0].geoJsonUrl;
+
+    selectedFloor: string = this.floors[0].geoJsonUrl;
     rooms: string[] = [];
     selectedRoom: string = '';
 
-    constructor(@Inject(PLATFORM_ID) private platformId: Object, private http: HttpClient) {
+    constructor(
+        @Inject(PLATFORM_ID) private readonly platformId: object,
+        private readonly http: HttpClient
+    ) {
         this.isBrowser = isPlatformBrowser(this.platformId);
-        this.loadRooms(this.selectedFloor);
+        this.fetchRooms(this.selectedFloor);
     }
 
-    loadRooms(geoJsonUrl: string) {
-        this.http.get(geoJsonUrl).subscribe((data: any) => {
-            const features = new GeoJSON().readFeatures(data);
-            this.rooms = features
+    fetchRooms(geoJsonUrl: string): void {
+        this.http.get(geoJsonUrl).subscribe((data: unknown) => {
+            const geoJsonFeatures = new GeoJSON().readFeatures(data);
+            this.rooms = geoJsonFeatures
                 .filter((feature: any) => feature.get('type') === 'Room')
                 .map((feature: any) => feature.get('name'));
         });
     }
 
-    onFloorChange(event: any) {
-        this.loadRooms(this.selectedFloor);
-        this.selectedRoom = "";
+    onFloorChange(newFloorUrl: string): void {
+        this.selectedFloor = newFloorUrl;
+        this.selectedRoom = '';
+        this.fetchRooms(newFloorUrl);
     }
 }
